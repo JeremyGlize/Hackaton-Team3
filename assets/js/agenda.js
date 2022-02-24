15bd1c8bcd253ac977b2774100701890e0adb26c
JeremyGlize <JeremGlize@gmail.com>
2022-02-24 15:55:47 +0100

    Push agenda page

---
+++ assets/js/agenda.js
@@ -0,0 +1,28 @@
+// ------ Carousel ---------
+const carouselScrollAreaJg = document.querySelector(".carouselScrollAreaJg");
+const leftCarouselButtonJg = document.querySelector(".carouselButtonLeftJg");
+const rightCarouselButtonJg = document.querySelector(".carouselButtonRightJg");
+const carouselWrapperJg = document.querySelector(".carouselWrapperJg");
+
+leftCarouselButtonJg.addEventListener("click", () => scrollCarousel("left"));
+rightCarouselButtonJg.addEventListener("click", () => scrollCarousel("right"));
+
+function scrollCarousel(direction) {
+  if (direction === "left") {
+    carouselScrollAreaJg.scrollLeft -= carouselScrollAreaJg.clientWidth;
+  } else if (direction === "right") {
+    carouselScrollAreaJg.scrollLeft += carouselScrollAreaJg.clientWidth;
+  } else {
+    console.error("Invalid direction");
+  }
+}
+carouselWrapperJg.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(e) {
+  if (e.key === "ArrowLeft") {
+    scrollCarousel("left");
+  } else if (e.key === "ArrowRight") {
+    scrollCarousel("right");
+  }
+}
+// ------ Fin Carousel -----------
